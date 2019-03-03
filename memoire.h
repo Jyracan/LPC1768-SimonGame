817759d1ac1f7ca93c2a7a2e485b6d7ba44af376
jyra <cmoineau@enssat.fr>
2019-03-03 17:03:43 +0100

    Suppression of useless files

--- memoire.h
+++
@@ -1,5 +0,0 @@
-#include "lpc17xx_i2c.h"
-
-void init_i2c_eeprom(LPC_I2C_TypeDef *,FunctionalState,uint32_t);
-void i2c_eeprom_write(LPC_I2C_TypeDef*,uint16_t,uint8_t*, int);
-void i2c_eeprom_read(LPC_I2C_TypeDef*,uint16_t,uint8_t*, int);
